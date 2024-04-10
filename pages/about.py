2c34e67d6d654e97bfdece8ae1417863e4126bb7
rafaelcoelho1409 <rafaelcoelho1409@hotmail.com>
2024-04-10 00:16:23 -0300

    Revert "About Us page added"
    This reverts commit 411bda404805691c0433fbcd6437cb7c04c00ed6.

--- pages/about.py
+++ pages/about.py
@@ -1,4 +1,3 @@
-import datetime as dt
 import streamlit as st
 from streamlit_extras.grid import grid
 from streamlit_card import card
@@ -56,34 +55,49 @@ with st.expander(
     image_border_radius("assets/rafael_coelho_1.jpeg", 20, 80, 80, grid1)
     grid1.container()
     container1 = grid1.container()
-    container1.markdown(f"""<div style='font-size:25px'>
+    container1.markdown("""<div style='font-size:25px'>
     Rafael Coelho is a Brazilian Mathematics student 
     who is passionated for Data Science and Artificial Intelligence
-    and works in both areas for over {str(dt.datetime.now().year - 2020)} years, with solid knowledge in
+    and works in both areas for over three years, with solid knowledge in
     technologic areas such as Machine Learning, Deep Learning, Data Science,
     Computer Vision, Reinforcement Learning, NLP and others.<br><br>
     Recently, he worked in one of the Big Four companies for over a year.</div>
     """, unsafe_allow_html = True)
-    container1.divider()
+    #test = container1.columns(3)
+    #with test[0]:
     buttons = container1.columns(3)
-    buttons[0].link_button(
-        "Portfolio",
-        "https://rafaelcoelho.streamlit.app/",
-        type = "primary",
-        use_container_width = True
-    )
-    buttons[1].link_button(
-        "LinkedIn",
-        "https://www.linkedin.com/in/rafaelcoelho1409/",
-        type = "primary",
-        use_container_width = True
-    )
-    buttons[2].link_button(
-        "GitHub",
-        "https://github.com/rafaelcoelho1409/",
-        type = "primary",
-        use_container_width = True
-    )
+    buttons[0].markdown("""
+    <div>
+    <h1>
+    <a 
+        style='text-align:center;'
+        href='https://rafaelcoelho.streamlit.app/'>
+    Portfolio
+    </a>
+    </h1>
+    </div>""", unsafe_allow_html = True)
+    #with test[1]:
+    buttons[1].markdown("""
+    <div>
+    <h1>
+    <a 
+        style='text-align:center;'
+        href='https://www.linkedin.com/in/rafaelcoelho1409/'>
+    LinkedIn
+    </a>
+    </h1>
+    </div>""", unsafe_allow_html = True)
+    #with test[2]:
+    buttons[2].markdown("""
+    <div>
+    <h1>
+    <a
+        style='text-align:center;'
+        href='https://github.com/rafaelcoelho1409/'>
+    GitHub
+    </a>
+    </h1>
+    </div>""", unsafe_allow_html = True)
 ####################################
 
 
